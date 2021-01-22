d2a6ad3d5dbdef330418b08e06edbe71abcb1b3c
luis-quan <xiaoquan1990@aliyun.com>
2021-01-22 17:10:05 +0800

    添加裸包接受

--- codec/msgcodec.go
+++ codec/msgcodec.go
@@ -1,6 +1,8 @@
 package codec
 
 import (
+	"reflect"
+
 	"github.com/luis-quan/cellnet"
 )
 
@@ -34,7 +36,19 @@ func DecodeMessage(msgid int, data []byte) (interface{}, *cellnet.MessageMeta, e
 
 	// 消息没有注册
 	if meta == nil {
-		return nil, nil, cellnet.NewErrorContext("msg not exists", msgid)
+		//转换成裸包
+		// Codec Codec        // 消息用到的编码
+		// Type  reflect.Type // 消息类型, 注册时使用指针类型
+		// ID int // 消息ID (二进制协议中使用)
+
+		msg := &cellnet.RawPacket{}
+		msg.MsgID = msgid
+		msg.MsgData = data
+		meta = new(cellnet.MessageMeta)
+		meta.ID = msgid
+		meta.Type = reflect.TypeOf((*cellnet.RawPacket)(nil)).Elem()
+		return msg, meta, nil
+		//return nil, nil, cellnet.NewErrorContext("msg not exists", msgid)
 	}
 
 	// 创建消息
